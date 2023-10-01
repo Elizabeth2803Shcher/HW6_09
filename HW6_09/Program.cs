ffb2a55820e996e46b7248dd6ccd4a3ff4397a80
Elizabeth2803 <elizaart2801@gmail.com>
2023-10-01 21:49:06 +0200

    Добавьте файлы проекта.

---
+++ HW6_09/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nubmer = "456";
+int numberTwo = int.Parse(nubmer);
+Console.WriteLine(numberTwo.GetType());
+string newNumber = numberTwo.ToString();
